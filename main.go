dc9b77df362538b4af71cba1c9637d19b2804b87
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2019-02-14 02:00:16 -0500

    server running

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main(){
-	
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", saludar)
+	http.ListenAndServe(":8080", nil)
+}
+
+func saludar(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
 }

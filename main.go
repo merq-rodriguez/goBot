c3de54df75fe52e80aff35e695da054e1c5344e5
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2019-02-14 02:40:56 -0500

    feat creation of server with connection tls and configuration

--- main.go
+++ main.go
@@ -1,12 +1,50 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+)
+
+type Config struct {
+	Port    string `json:"PORT"`
+	CertPem string `json:"CERT_PEM"`
+	KeyPem  string `json:"KEY_PEM"`
+}
+
+//Server configurations
+var config Config
+
+
 
 func main() {
+	loadConfig()
 	http.HandleFunc("/", saludar)
-	http.ListenAndServe(":8080", nil)
+
+	log.Printf("Server running in https://localhost%s", config.Port)
+	err := http.ListenAndServeTLS(config.Port, config.CertPem, config.KeyPem, nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func saludar(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello World!"))
+}
+
+func loadConfig(){
+	log.Println("Starting reading of the configuration file")
+	
+	bytes, err := ioutil.ReadFile("./config.json")
+	if err != nil {
+		log.Fatalf("Err reading file: %v", err)
+	}
+	err = json.Unmarshal(bytes, &config)
+	if err != nil {
+		log.Fatalf("Err converting file: %v",err)
+	}
+
+	log.Println("Ending reading of the configuration file")
+
 }
